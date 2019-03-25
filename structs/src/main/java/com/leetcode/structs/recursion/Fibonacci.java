759a2c98f44053cf872340588a7c4e114c17ee71
liuhaoeric <liuhaoeric@didichuxing.com>
2019-03-25 11:50:43 +0800

    Fibonacci.java

--- structs/src/main/java/com/leetcode/structs/recursion/Fibonacci.java
+++ structs/src/main/java/com/leetcode/structs/recursion/Fibonacci.java
@@ -1,19 +1,41 @@
 package com.leetcode.structs.recursion;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author <a href=mailto:ericliu@fivewh.com>ericliu</a>,Date:2019/3/25
  */
 public class Fibonacci {
 
-    public int cal(int n){
-        if(n==1 || n==2){
+    private Map<Integer, Long> map = new HashMap<>();
+
+    /**
+     * 递推公式f(n)=f(n-1)+f(n-2)，结果：n==1 return 1 n==2 return 1
+     * @param n
+     * @return
+     */
+    public long cal(int n) {
+        //递归结果
+        if (n == 1 || n == 2) {
             return 1;
         }
 
-        return cal(n-1)+cal(n-2);
+        //记录结果避免重复计算
+        if (map.containsKey(n)) {
+            return map.get(n);
+        }
+
+        //计算
+        long ret = cal(n - 1) + cal(n - 2);
+        map.put(n, ret);
+        return ret;
     }
 
     public static void main(String[] args) {
-        System.out.println(new Fibonacci().cal(5));
+        long now=System.currentTimeMillis();
+        System.out.println(new Fibonacci().cal(100));
+
+        System.out.println("spent===>"+(System.currentTimeMillis()-now));
     }
 }
