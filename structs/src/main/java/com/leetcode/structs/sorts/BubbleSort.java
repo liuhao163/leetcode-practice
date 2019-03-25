c8ec5ca5583b24b2cf6db8d53e84fd983fe89222
liuhao163 <liu67224657@163.com>
2019-03-25 23:08:36 +0800

    BubbleSort.java InsertSort.java

--- structs/src/main/java/com/leetcode/structs/sorts/BubbleSort.java
+++ structs/src/main/java/com/leetcode/structs/sorts/BubbleSort.java
@@ -7,38 +7,19 @@ import java.util.Arrays;
  */
 public class BubbleSort {
 
-    public int[] sortDesc(int[] a) {
-        for (int i = 0; i < a.length; i++) {
-            boolean needBubble=false;
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i] < a[j]) {
-                    int tmp=a[i];
-                    a[i]=a[j];
-                    a[j]=tmp;
-                    needBubble=true;
-                }
-            }
-            if(!needBubble){
-                return a;
-            }
-        }
-
-        return a;
-    }
-
     public int[] sort(int[] array) {
         //第一层循环没循环一次冒泡一次，如果某一次没有发生位置变化说明数组已经完全有序，停止冒泡needBuuble
         for (int i = 0; i < array.length; i++) {
-            boolean needBuuble=false;
+            boolean needBuuble = false;
             for (int j = i + 1; j < array.length; j++) {
                 int tmp = array[j];
                 if (array[i] > array[j]) {
                     array[j] = array[i];
                     array[i] = tmp;
-                    needBuuble=true;
+                    needBuuble = true;
                 }
             }
-            if(!needBuuble){
+            if (!needBuuble) {
                 return array;
             }
         }
@@ -49,6 +30,6 @@ public class BubbleSort {
 
         int[] array = new int[]{80, 3, 4, 8, 9, 100, 101, 102, 103};
 
-        System.out.println(Arrays.toString(new BubbleSort().sortDesc(array)));
+        System.out.println(Arrays.toString(new BubbleSort().sort(array)));
     }
 }
