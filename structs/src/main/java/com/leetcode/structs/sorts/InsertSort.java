b9409267f07aa8252c2285a5e7322b36f9a18a68
liuhao163 <liu67224657@163.com>
2019-03-26 22:39:33 +0800

    BubbleSort.java InsertSort.java

--- structs/src/main/java/com/leetcode/structs/sorts/InsertSort.java
+++ structs/src/main/java/com/leetcode/structs/sorts/InsertSort.java
@@ -12,7 +12,7 @@ public class InsertSort {
             int tmp = a[i];
             int j = i - 1;
             for (; j >= 0; j--) {
-                if (a[j] > tmp) {
+                if (a[j] < tmp) {
                     a[j + 1] = a[j];
                 } else {
                     break;
