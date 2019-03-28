b0f8316d112a907447e97f8551d2b26b99dc8eb3
liuhao163 <liu67224657@163.com>
2019-03-28 22:53:02 +0800

    MergeSort.java

--- structs/src/main/java/com/leetcode/structs/sorts/QuickSort.java
+++ structs/src/main/java/com/leetcode/structs/sorts/QuickSort.java
@@ -13,35 +13,44 @@ public class QuickSort {
         System.out.println(Arrays.toString(array));
     }
 
+    /**
+     * 获取数组的一个标志位（交换区点一般是数组最后一位，小于这个点的在左边，大于的在右边）
+     *
+     * @param a
+     * @param s
+     * @param e
+     */
     private void sort(int[] a, int s, int e) {
         if (s >= e) {
             return;
         }
 
         int poivt = poivt(a, s, e);
-        sort(a, s, poivt - 1);//todo 重点
-        sort(a, poivt, e);//todo 重点
+        sort(a, s, poivt - 1);//排序取到分区点后，分区点-1，参与排序
+        sort(a, poivt, e);//从分区点到终点排序
     }
 
     private int poivt(int[] a, int s, int e) {
-        int swapIdx = s;
-        int r = a[e];
+        int swapIdx = s;//交换区为s，随着数组循环移动
+        int flag = a[e];//设置flag即最后交换区
 
-        for (int i = s; i <= e - 1; i++) {//重点
-            if (a[i] <= r) {//todo 重点
+        //s-->e-1,循环到交换区前
+        for (int i = s; i <= e - 1; i++) {
+            //即时小于也要交换，让swpIdx后移
+            if (a[i] <= flag) {
                 int tmp = a[i];
                 a[i] = a[swapIdx];
                 a[swapIdx] = tmp;
-                swapIdx++;//扩大
+                swapIdx++;//扩大前半部分
             }
         }
 
+        //最后将flag移动到此次poivt点并且返回交换区点
         int tmp = a[swapIdx];
-        a[swapIdx] = r;
+        a[swapIdx] = flag;
         a[e] = tmp;
 
         return swapIdx;
     }
-
 
 }
