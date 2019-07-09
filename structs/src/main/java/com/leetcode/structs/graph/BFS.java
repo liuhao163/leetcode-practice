ba61d96abce9cd668a2e0ddac98aa8783f71b11f
liuhaoeric <liuhaoeric@didiglobal.com>
2019-07-09 15:00:03 +0800

    dfs

--- structs/src/main/java/com/leetcode/structs/graph/BFS.java
+++ structs/src/main/java/com/leetcode/structs/graph/BFS.java
@@ -9,33 +9,41 @@ public class BFS {
 
     private List<Vertex> list = new ArrayList<>();
 
-    private LinkedList<Vertex> queue = new LinkedList<>();
-    private Set<Vertex> visited = new HashSet<>();
-    private Map<Integer, Integer> path = new HashMap<>();
 
     public BFS(List<Vertex> list) {
         this.list = list;
+    }
+
+    public void search(Vertex end) {
+        //借用队列，将下一个要查找的点入队列
+        LinkedList<Vertex> queue = new LinkedList<>();
+        //记录搜索过的点，找到值后跳过
+        Set<Vertex> visited = new HashSet<>();
+        //记录路径key是当前的点的下标，value是来源，打印时候通过递归层层找到最开始的起点
+        Map<Integer, Integer> path = new HashMap<>();
+
+        //添加起点坐标
+        queue.add(list.get(0));
+        visited.add(list.get(0));
+        //初始化路径
         list.forEach(v -> {
             path.put(v.getVal(), -1);
         });
 
-    }
-
-    public void search(Vertex end) {
-        queue.add(list.get(0));
-        visited.add(list.get(0));
-
+        //第一层结束后才开始第二层
         while (queue.size() > 0) {
+            //队列出列
             Vertex v = queue.poll();
             for (Vertex adjV : v.getAdj()) {
                 if (visited.contains(adjV)) {
                     continue;
                 }
 
+                //修改path和visited
                 visited.add(adjV);
                 path.put(adjV.getVal(), v.getVal());
                 if (adjV.equals(end)) {
-                    //print
+                    //找到终点打印
                     GraphUtil.printPath(path, list.get(0), end.getVal());
                     return;
                 }
@@ -52,6 +60,7 @@ public class BFS {
      * @return
      */
     public List<Vertex> searchByDegree(List<Vertex> list, int degree) {
+        //记录当前是第几度关系的容器
         LinkedList<DegreeVertex> queue = new LinkedList<>();
         Set<Vertex> visited = new HashSet<>();
 
@@ -59,13 +68,14 @@ public class BFS {
         queue.add(new DegreeVertex(i, list.get(0)));
         visited.add(list.get(0));
 
-
         List<Vertex> ret = new ArrayList<>();
         while (queue.size() > 0) {
             DegreeVertex degreeVertex = queue.poll();
+            //超过度直接返回
             if (degreeVertex.getDegree() > degree) {
                 break;
             }
+
             i = degreeVertex.getDegree() + 1;
             for (Vertex adjV : degreeVertex.vertex.getAdj()) {
                 if (visited.contains(adjV)) {
--- structs/src/main/java/com/leetcode/structs/graph/DFS.java
+++ structs/src/main/java/com/leetcode/structs/graph/DFS.java
@@ -6,21 +6,23 @@ import java.util.*;
  * @author <a href=mailto:ericliu@fivewh.com>ericliu</a>,Date:2019/7/8
  */
 public class DFS {
+
     private List<Vertex> graph = new ArrayList<>();
+
+    //利用visited和path
     private Set<Vertex> visited = new HashSet<>();
     private Map<Integer, Integer> path = new HashMap<>();
-
+    //用于结束递归的标记位发现后置为true停止递归
     private boolean found = false;
 
     public DFS(List<Vertex> graph) {
         this.graph = graph;
-
-        graph.forEach(vertex -> {
-            path.put(vertex.getVal(), -1);
-        });
     }
 
     public void search(Vertex end) {
+        graph.forEach(vertex -> {
+            path.put(vertex.getVal(), -1);
+        });
         search(graph.get(0), end);
         GraphUtil.printPath(path, graph.get(0), end.getVal());
     }
