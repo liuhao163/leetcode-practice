55dcdf11fd956a959d1f80758c22b7bd0d556675
liuhaoeric <liuhaoeric@didichuxing.com>
2020-06-27 16:29:42 +0800

    lc41 方法2

--- leetcode/src/main/java/lc41/Solution.java
+++ leetcode/src/main/java/lc41/Solution.java
@@ -5,17 +5,13 @@ package lc41;
  */
 public class Solution {
     public static void main(String[] args) {
-//        System.out.println(new Solution().firstMissingPositive(new int[]{1000,-5}));
-//        System.out.println(new Solution().firstMissingPositive(new int[]{1,2,0}));
-        System.out.println(new Solution().firstMissingPositive(new int[]{1}));
+//        System.out.println(new Solution().firstMissingPositive2(new int[]{1000,-5}));
+//        System.out.println(new Solution().firstMissingPositive2(new int[]{1,2,0}));
+        System.out.println(new Solution().firstMissingPositive2(new int[]{3,4,1,-1}));
 
     }
 
     public int firstMissingPositive(int[] nums) {
-        if (nums.length == 0) {
-            return 1;
-        }
-
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] <= 0) {
                 nums[i] = nums.length + 1;
@@ -25,16 +21,36 @@ public class Solution {
         for (int i = 0; i < nums.length; i++) {
             int v = Math.abs(nums[i]);
             if (v <= nums.length) {
-                nums[v - 1] = -Math.abs(nums[v-1]);
+                nums[v - 1] = -Math.abs(nums[v - 1]);
             }
         }
 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
-                return i + 1;//
+                return i + 1;
             }
         }
 
-        return nums.length+1;
+        return nums.length + 1;
+    }
+
+
+    public int firstMissingPositive2(int[] nums) {
+        int n=nums.length;
+        for (int i = 0; i < n; i++) {
+            while(nums[i]>0 && nums[i]<=n && nums[i]!=nums[nums[i]-1]){
+                int t=nums[i];
+                nums[i]=nums[nums[i]-1];
+                nums[t-1]=t;
+            }
+        }
+
+        for (int i=0;i<n;i++){
+            if (nums[i]!=i+1){
+                return i+1;
+            }
+        }
+
+        return n+1;
     }
 }
