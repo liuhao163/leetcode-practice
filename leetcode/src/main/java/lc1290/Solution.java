551458bf3435b3466e048b38da7da3343c4ac59c
liuhaoeric <liuhaoeric@didichuxing.com>
2020-06-28 19:38:57 +0800

    Consumer.java Producer.java

--- leetcode/src/main/java/lc1290/Solution.java
+++ leetcode/src/main/java/lc1290/Solution.java
@@ -1,17 +1,37 @@
 package lc1290;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author <a href=mailto:ericliu@fivewh.com>ericliu</a>,Date:2020/6/27
  */
 public class Solution {
 
     public static void main(String[] args) {
-
+        ListNode l1=new ListNode(1);
+        ListNode l2=new ListNode(0);
+        ListNode l3=new ListNode(1);
+        l1.next=l2;
+        l2.next=l3;
+//        1->0->1
+        System.out.println(new Solution().getDecimalValue(l1));
     }
 
     public int getDecimalValue(ListNode head) {
+        ListNode cur = head;
+        List<Integer> list = new ArrayList<>();
+        int ret = 0;
+        while (cur != null) {
+            list.add(cur.val);
+            cur = cur.next;
+        }
 
-        return 0;
+        for (int i = 0; i < list.size(); i++) {
+            ret += list.get(i) << (list.size() - i-1);
+        }
+
+        return ret;
     }
 
 
